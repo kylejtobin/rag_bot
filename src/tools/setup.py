045c97736ccd60ce0fd2b9d3263da6986d1db0c6
kyle-aurelius <kyle@aureliusmind.ai>
2023-09-24 17:40:39 -0700

    updated prompt template and readme

--- src/tools/setup.py
+++ src/tools/setup.py
@@ -3,7 +3,6 @@
 # Primary Components 
 from langchain import SerpAPIWrapper
 from langchain.agents import Tool
-from langchain.tools import tool
 
 from src.tools.doc_search import DocumentSearch
 
@@ -20,29 +19,38 @@ class ToolSetup:
     def setup_tools() -> list:
         """
         Static method to initialize and return a list of tools for the agent.
-        
         Returns:
-        - list: A list of initialized tools for agent's use. 
+        - list: A list of initialized tools for agent's use.
         """
         
         # Create an instance of the SerpAPI wrapper for Google searches.
-        search = SerpAPIWrapper()
+        SerpAPI = SerpAPIWrapper()
         
-        @tool("search_techdocs")
+        def search_web(query: str) -> str:
+            """
+            This function uses the SerpAPI wrapper to conduct a Google search and returns the raw search results.
+            """
+            return SerpAPI.run(query)
+        
         def search_techdocs(query: str, collection: str="techdocs") -> str:
-            """This tool enables the querying of a specialized vector store named ‘TechDocs,’ a repository where users archive valuable technical documentation they have encountered. It is particularly beneficial when engaging with technical subjects or when involved in coding activities. Utilize this search tool to scrutinize the vector store for pertinent context when addressing technical inquiries or tasks. If a term from the user input is unfamiliar but appears to be technical in nature, it is imperative to consult ‘TechDocs’ to ascertain whether relevant information or context is available therein. For your awareness, the information provided is sourced from ‘TechDocs,’ and we will refer to this source for any related queries."""
+            """
+            This tool enables the querying of a specialized vector store named ‘TechDocs,’ a repository where users archive valuable technical documentation they have encountered. It is particularly beneficial when engaging with technical subjects or when involved in coding activities. Utilize this search tool to scrutinize the vector store for pertinent context when addressing technical inquiries or tasks. If a term from the user input is unfamiliar but appears to be technical in nature, it is imperative to consult ‘TechDocs’ to ascertain whether relevant information or context is available therein. For your awareness, the information provided is sourced from ‘TechDocs,’ and we will refer to this source for any related queries.
+            """
             
             search = DocumentSearch(collection, query)
             results = search.search_documents()
-
-            return results
             
+            return results
         
         return [
             Tool(
-                name="Search", 
-                func=search.run,
-                description="The Search tool uses SerpAPI to conduct Google searches. It retrieves raw search results without any inherent interpretation. Use this tool to fetch real-time information and analyze the results."
+                name="search_web",
+                func=search_web,
+                description="This is a tool that conducts Google searches via the SerpAPI to retrieve real-time search results programmatically, allowing for efficient extraction and analysis of search data to obtain current and relevant web information for a given query."
             ),
-            search_techdocs
+            Tool(
+                name="search_techdocs",
+                func=search_techdocs,
+                description="This tool enables the querying of a specialized vector store named ‘TechDocs,’ a repository where users archive valuable technical documentation they have encountered."
+            )
         ]
