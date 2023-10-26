e55de8bc2db2a0a72262769cfeadca577b521c4f
chrishart0 <christianphart0@gmail.com>
2023-10-25 23:14:37 -0400

    Make integration test pass consistent

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,17 +1,18 @@
 import httpx
 import pytest
 
+# ToDo: Support multi-chat sessions and then make integration tests run in parallel
 
 # Define the base URL of your FastAPI application
 BASE_URL = "http://localhost:8000"
 
 
 def test_basic_chat_hi_no_rag():
-    with httpx.Client(timeout=10.0) as client: # Chat response can take a while
-        response = client.post(f"{BASE_URL}/chat/", json={ "user_input": "Hi" })
+    with httpx.Client(timeout=30.0) as client: # Chat response can take a while
+        response = client.post(f"{BASE_URL}/chat/", json={ "user_input": "Simply respond with hi" })
         assert response.status_code == 200
 
 def test_chat_with_rag():
-    with httpx.Client(timeout=30.0) as client: # Chat response can take a while
+    with httpx.Client(timeout=45.0) as client: # Chat response can take a while
         response = client.post(f"{BASE_URL}/chat/", json={ "user_input": "What are the basic steps to get rag_bot up and running?" })
         assert response.status_code == 200
