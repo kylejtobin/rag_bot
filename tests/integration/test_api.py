5d16b36a4db4e0b161c84c5a9c44b3bdf77e84a7
chrishart0 <christianphart0@gmail.com>
2023-10-25 23:57:21 -0400

    Add integration tests to github action

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,5 +1,6 @@
 import httpx
 import pytest
+import time
 
 # ToDo: Support multi-chat sessions and then make integration tests run in parallel
 
@@ -8,11 +9,13 @@ BASE_URL = "http://localhost:8000"
 
 
 def test_basic_chat_hi_no_rag():
+    time.sleep(2) # Give bot time between tests. Need to add support from multi-chat
     with httpx.Client(timeout=30.0) as client: # Chat response can take a while
         response = client.post(f"{BASE_URL}/chat/", json={ "user_input": "Simply respond with hi" })
         assert response.status_code == 200
 
 def test_chat_with_rag():
+    time.sleep(2) # Give bot time between tests
     with httpx.Client(timeout=45.0) as client: # Chat response can take a while
         response = client.post(f"{BASE_URL}/chat/", json={ "user_input": "What are the basic steps to get rag_bot up and running?" })
         assert response.status_code == 200
